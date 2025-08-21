2388db92a671a0d5f15f7dbb13978e9456a471f4
Joshua-Seah <194608515+Joshua-Seah@users.noreply.github.com>
2025-08-21 10:14:18 +0800

    Level 2
    Added list function

--- src/main/java/APleaseBot.java
+++ src/main/java/APleaseBot.java
@@ -1,6 +1,34 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class APleaseBot {
+
+    public static class Task {
+        // Fields
+        protected String name;
+        protected boolean done;
+
+
+
+        // constructor
+        public Task(String name) {
+            this.name = name;
+            this.done = false;
+        }
+
+
+
+        // methods
+        public void markDone() {
+            this.done = true;
+        }
+
+        public String toString() {
+            return (done ? "[X]" : "[ ]") + " " + name;
+        }
+    }
+
     public static void main(String[] args) {
 
         String line = "________________________________________\n";
@@ -11,6 +39,10 @@ public class APleaseBot {
 
         String close = "Bye. Hope to see you again soon!\n";
 
+        // Initialise list
+        Task[] taskList = new Task[100];
+        int itemCount = 0;
+
 
 
         // Inputs
@@ -19,13 +51,23 @@ public class APleaseBot {
         Scanner scanner = new Scanner(System.in);
 
 
+
         while(true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(line + close + line);
                 break;
+            } else if(input.equals("list")) {
+                String output = "";
+                for (int j = 0; j < itemCount; j++) {
+                    output += j+1 + ". " + taskList[j] + "\n";
+                }
+                System.out.println(line + output + line);
             } else {
-                System.out.println(line + input + "\n" + line);
+                taskList[itemCount] = new Task(input);
+                itemCount++;
+                System.out.println(line + "added: " + input + "\n" + line);
             };
         }
     }
